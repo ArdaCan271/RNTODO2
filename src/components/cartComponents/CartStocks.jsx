e7626062a6fe7001ebe945d1368f15ff736913e8
ardacan271 <ardacan271@gmail.com>
2024-08-20 17:55:15 +0300

    .

--- src/components/cartComponents/CartStocks.jsx
+++ src/components/cartComponents/CartStocks.jsx
@@ -16,8 +16,6 @@ const CartStocks = ({ navigation, productList }) => {
 
   const userData = useSelector((state) => state.userData.data);
 
-  console.log(productList);
-
   return (
     <View style={styles.cartStocksSection}>
       <View style={styles.cartStocksContainer}>
